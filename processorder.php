e93359ded36a10f276341d73643b97085a435763
Song Yonghong <yhsong@linx-info.com>
2015-06-29 17:27:37 +0800

    Save the form data to the regular files

--- processorder.php
+++ processorder.php
@@ -1,3 +1,14 @@
+<?php
+    /* Create short variable names */
+    $tireqty = $_POST[ 'tireqty' ];
+    $oilqty = $_POST[ 'oilqty' ];
+    $sparkqty = $_POST[ 'sparkplugs' ];
+    $find = $_POST[ 'find' ];
+    $address = $_POST[ 'address' ];
+    $DOCUMENT_ROOT = $_SERVER[ 'DOCUMENT_ROOT' ];
+    $date = date('H:i, jS F Y');
+?>
+
 <html>
 <head>
     <title>Bob 's Auto Parts - Order Results</title>
@@ -6,15 +17,7 @@
 <h1>Bob 's Auto Parts</h1>
 <h2>Order Results</h2>
 <?php
-    echo "<p>Order processed at ";
-    echo date('H:i, jS F Y');
-    echo ".</p>";
-
-    /* Create short variable names */
-    $tireqty = $_POST[ 'tireqty' ];
-    $oilqty = $_POST[ 'oilqty' ];
-    $sparkqty = $_POST[ 'sparkplugs' ];
-    $find = $_POST[ 'find' ];
+    echo '<p>Order processed at ' .$date. '</p>';
 
     $totalqty = 0;
     $totalqty = $tireqty + $oilqty + $sparkqty;
@@ -40,7 +43,7 @@
 	if ($sparkqty > 0)
 	    echo $sparkqty. ' spark plugs<br />';
 
-	echo 'Items ordered: '.$totalqty.'<br />';
+	echo 'Items ordered: ' .$totalqty. '<br />';
     }
     $totalamount = 0.00;
 
@@ -52,11 +55,32 @@
 		+ $oilqty * OILPRICE 
 		+ $sparkqty * SPARKPRICE;
 
-    echo "Subtotal: $".number_format($totalamount,2)."<br />";
+    echo 'Subtotal: $' .number_format($totalamount,2). '<br />';
 
     $taxrate = 0.10;	//local scales tax is 10%
     $totalamount = $totalamount * (1 + $taxrate);
-    echo "Total including tax: $".number_format($totalamount,2)."<br />";
+    echo 'Total including tax: $' .number_format($totalamount,2). '<br />';
+
+    echo '<p>Address to the ship to is ' .$address. '</p>';
+    $outputstring= $date. "\t" .$tireqty. " tires \t" .$oilqty. " oil \t"
+	.$sparkqty. " spark plugs\t\$" .$totalamount. "\t" .$address. "\n";
+
+    //open file for appending
+    @ $fp = fopen("$DOCUMENT_ROOT/order.txt", 'ab'); 
+
+    flock($fp, LOCK_EX);
+
+    if (!$fp){
+	echo "<p><strong> Your order could not be processed at this time.
+	    Please try again later.</strong></p></body></html>";
+	exit;
+    }
+
+    fwrite($fp, $outputstring, strlen($outputstring));
+    flock($fp, LOCK_UN);
+    fclose($fp);
+
+    echo "Order written.";
 
     switch($find){
 	case "a" :
