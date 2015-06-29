392f9a9a49c37871c4ca8305ca0de476bdb1ae5d
Song Yonghong <yhsong@linx-info.com>
2015-06-29 15:01:25 +0800

    Test for statement

--- freight.php
+++ freight.php
@@ -6,13 +6,20 @@
     <td bgcolor= "#CCCCC"align= "center">Cost</td>
 </tr>
 <?php
-    $distance = 50;
+/*    $distance = 50;
     while($distance <= 250){
 	echo "<tr>
 	    <td align=\"right\">".$distance."</td>
 	    <td align=\"right\">".($distance / 10)."</td>
 	    </tr>\n";
     $distance += 50;
+    }
+*/
+    for ($distance = 50; $distance <= 250; $distance += 50){
+	echo "<tr>
+	    <td align=\"right\">".$distance."</td>
+	    <td align=\"right\">".($distance / 10)."</td>
+	    </tr>";
     }
 ?>
 </table>
