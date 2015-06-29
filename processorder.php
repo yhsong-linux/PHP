62c310aea593d3b2ef6607ba90e4c5af1c52624d
Song Yonghong <yhsong@linx-info.com>
2015-06-29 11:45:50 +0800

    Form total amount is calculated

--- processorder.php
+++ processorder.php
@@ -1,21 +1,43 @@
 <html>
 <head>
-	<title>Bob's Auto Parts - Order Results</title>
+    <title>Bob 's Auto Parts - Order Results</title>
 </head>
 <body>
-<h1>Bob's Auto Parts</h1>
+<h1>Bob 's Auto Parts</h1>
 <h2>Order Results</h2>
 <?php
-	echo '<p>Order processed at ';
-	echo date('H:i, jS F Y');
-	echo '</p>';
-	$tireqty = $_POST[ 'tireqty' ];
-	$oilqty = $_POST[ 'oilqty' ];
-	$sparkplugs = $_POST[ 'sparkplugs' ];
-	echo '<p>Your order is as follow:</p>';
-	echo $tireqty. ' tires<br />';
-	echo $oilqty. ' bottles of oil<br />';
-	echo $sparkplugs. ' spark plugs<br />';
+    echo "<p>Order processed at ";
+    echo date('H:i, jS F Y');
+    echo ".</p>";
+
+    /* Create short variable names */
+    $tireqty = $_POST[ 'tireqty' ];
+    $oilqty = $_POST[ 'oilqty' ];
+    $sparkqty = $_POST[ 'sparkqty' ];
+/*  echo '<p>Your order is as follows: </p>';
+    echo $tireqty. ' tires<br />';
+    echo $oilqty. ' bottles of oil<br />';
+    echo $sparkqty. ' spark plugs<br />'; 
+*/
+
+    $totalqty = 0;
+    $totalqty = $tireqty + $oilqty + $sparkqty;
+    echo "Items ordered: ".$totalqty."<br />";
+    $totalamount = 0.00;
+
+    define('TIREPRICE', 100);
+    define('OILPRICE', 10);
+    define('SPARKPRICE', 4);
+
+    $totalamount = $tireqty * TIREPRICE 
+		+ $oilqty * OILPRICE 
+		+ $sparkqty * SPARKPRICE;
+
+    echo "Subtotal: $".number_format($totalamount,2)."<br />";
+
+    $taxrate = 0.10;	//local scales tax is 10%
+    $totalamount = $totalamount * (1 + $taxrate);
+    echo "Total including tax: $".number_format($totalamount,2)."<br />";
 ?>
 </body>
 </html>
