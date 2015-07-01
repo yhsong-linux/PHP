f717795eadd2ca79c49da0d4ef5a45ed18de259c
Song Yonghong <yhsong@linx-info.com>
2015-07-01 16:40:25 +0800

    To random sort an array

---
+++ bods_font_page.php
@@ -0,0 +1,28 @@
+<?php
+	$pictures = array('xpn.png', 'xqf_32x32.png', 'xrced.png', 'xtide.png',
+			'yabause.png', 'yakuake.png', 'zim.png', 'yoshimi.png'
+	);
+
+	shuffle($pictures);
+?>
+<html>
+<head>
+	<title>Bob's Auto Parts</title>
+</head>
+<body>
+	<h1>Bob's Auto Parts</h1>
+	<div align="center">
+		<table width="100%">
+			<tr>
+				<?php
+					for ($i = 0; $i < 3; $i++){
+						echo "<td align=\"center\"><img src=\"icon/";
+						echo $pictures[$i];
+						echo "\" /></td>";
+					}
+				?>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
