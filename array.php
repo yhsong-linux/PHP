703b863d3682a2112107947e39a5a5ecffa3813f
Song Yonghong <yhsong@linx-info.com>
2015-07-01 13:55:01 +0800

    Add access an associative array of two other methods: each and list

--- array.php
+++ array.php
@@ -1,5 +1,16 @@
 <?php
-	$price = array('Tires' => 100, 'oil' => 10, 'spark plug' => 4);
-	foreach ($price as $key => $value)
-		echo $key. " - " .$value. "<br />";
+	$prices = array('Tires' => 100, 'oil' => 10, 'spark plug' => 4);
+	foreach ($prices as $key => $value)
+		echo $key. ' - ' .$value. '<br />';
+	reset($prices);
+	while ($element = each($prices)){
+		echo $element[ 'key' ];
+		echo ' - ';
+		echo $element[ 'value' ];
+		echo '<br />';
+	}
+	reset($prices);
+	while (list($product, $price) = each($prices)){
+		echo $product. ' - ' .$price. '<br />';
+	}
 ?>
