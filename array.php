87420631e04d5073408adbeaa1614ba1987fb4fa
Song Yonghong <yhsong@linx-info.com>
2015-07-01 14:51:04 +0800

    Add two-dimensional associative array access methods

--- array.php
+++ array.php
@@ -2,15 +2,30 @@
 	$prices = array('Tires' => 100, 'oil' => 10, 'spark plug' => 4);
 	foreach ($prices as $key => $value)
 		echo $key. ' - ' .$value. '<br />';
+
 	reset($prices);
+
 	while ($element = each($prices)){
 		echo $element[ 'key' ];
 		echo ' - ';
 		echo $element[ 'value' ];
 		echo '<br />';
 	}
+
 	reset($prices);
+
 	while (list($product, $price) = each($prices)){
 		echo $product. ' - ' .$price. '<br />';
+	}
+
+	$products = array(array('Code' => 'TIR', 'Description' => 'Tires', 'Price' => 100),
+			array('Code' => 'OIL', 'Description' => 'oil', 'Price' => 10),
+			array('Code' => 'SPK', 'Description' => 'spaks plugs', 'Price' => 4)
+	);
+	for ($row = 0; $row < 3; $row++){
+		while (list($key, $value) = each($products[$row])) {
+			echo '|' .$value;
+		}
+		echo '|<br />';
 	}
 ?>
